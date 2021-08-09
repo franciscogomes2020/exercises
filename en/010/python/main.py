5fabb001ebffcc0897c3df4c298c85a61c2aa33d
Francisco Gomes <71292537+franciscogomes2020@users.noreply.github.com>
2021-08-08 23:08:50 -0300

    :sparkles: Python example 010 created

---
+++ en/010/python/main.py
@@ -0,0 +1,9 @@
+# Create a program that reads how much money a person has in their wallet and displays how much Reais they can buy. Consider U$1.00 = R$3.27.
+
+dollar = float( input(' How much money do you have? ') )
+usdbrl = 3.27
+print(' you can buy {:.2f} Reals'.format(dollar * usdbrl ) )
+
+# Output -->
+# How much money do you have? 2.00
+# you can buy 6.54 Reals
